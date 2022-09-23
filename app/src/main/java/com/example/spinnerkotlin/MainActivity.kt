f731f5ef7daa9644fb5d85d9f6c2fdedd1890ed7
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2022-09-23 10:12:08 -0500

    Crear Spinner Parte II

--- app/src/main/java/com/example/spinnerkotlin/MainActivity.kt
+++ app/src/main/java/com/example/spinnerkotlin/MainActivity.kt
@@ -2,19 +2,36 @@ package com.example.spinnerkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import com.example.spinnerkotlin.databinding.ActivityMainBinding
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     private lateinit var mBinding: ActivityMainBinding
+    private lateinit var aaCountries: ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
-        val aaCountries = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item)
+        aaCountries = ArrayAdapter<String>(this,
+            android.R.layout.simple_spinner_dropdown_item)
+
+        aaCountries.addAll(listOf("Mexico", "Peru", "España"))
+        mBinding.spinnerCountries.onItemSelectedListener = this
         mBinding.spinnerCountries.adapter = aaCountries
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+        val countrySelected = aaCountries.getItem(position)
+        mBinding.tvSelected.text = countrySelected
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
